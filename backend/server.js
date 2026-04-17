9ede38277a8356fbfb879cc3a0be1c7a0ad4d011
bushra-marwa <bushra613m@gmail.com>
2026-04-18 02:32:30 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ if (!fs.existsSync('uploads')) {
 }
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: '*',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
